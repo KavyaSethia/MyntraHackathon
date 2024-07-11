56d15336a99d82a787977f47bc0950eeede448d9
KavyaSethia <104768912+KavyaSethia@users.noreply.github.com>
2024-07-11 13:26:40 +0530

    Initial commit

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://dbUser:dbUsage112@cluster0.s5dycnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "377cf428877c3498357b02e266ac44e704d17f832bc39c2f8fbf53d50904aff0"
+
+};
